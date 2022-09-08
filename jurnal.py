c7324db97f7eabb3977d7e4273a5a3f19b998819
yassgt <98251413+yassgt@users.noreply.github.com>
2022-09-09 00:43:28 +0700

    gas

--- jurnal.py
+++ jurnal.py
@@ -90,5 +90,5 @@ with col2:
     st.header("Hasil Joint(𝝀)")
     st.dataframe(ql.style.format(precision=3))
 
-st.bar_chart(df[['Joint(𝝀)','Joint(Q)']])
-col1, col2 = st.columns(2)
+    st.bar_chart(df[['Joint(𝝀)','Joint(Q)']])
+    col1, col2 = st.columns(2)
